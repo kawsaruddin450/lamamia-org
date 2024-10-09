2b70dfb4697001346aefa11ec600639ad248a136
kawsar-uddin <kawsaruddin450@gmail.com>
2024-10-09 23:44:41 +0600

    navbar and footer add in the layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/components/navbar/NavBar";
+import Footer from "@/components/footer/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar></NavBar>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
