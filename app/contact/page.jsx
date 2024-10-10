856c6ca328825967ae41cf95ec8eb05b6a6a2917
kawsar-uddin <kawsaruddin450@gmail.com>
2024-10-10 12:59:46 +0600

    complete contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import contactImg from "@/public/contact.png";
+import Image from 'next/image';
+import Button from '@/components/button/Button';
 
 const Contact = () => {
     return (
         <div>
-            Contact
+            <h1 className="text-6xl font-bold mb-28 text-center">Let`s Keep in Touch...</h1>
+            <div className="flex gap-24">
+                <div className="flex-1 relative h-[400px]">
+                    <Image className="w-3/4 object-contain" fill={true} src={contactImg} alt="Contact Page Image" />
+                </div>
+                <form className="flex-1 flex flex-col gap-3">
+                    <input className="bg-inherit text-white p-4 border-2 rounded border-slate-200" type="text" placeholder="Name" />
+                    <input className="bg-inherit text-white p-4 border-2 rounded border-slate-200" type="email" placeholder="Email" />
+                    <textarea className="bg-inherit text-white p-4 border-2 rounded border-slate-200" placeholder="Message" rows={5}></textarea>
+                    <Button url="#" text="Send" />
+                </form>
+            </div>
         </div>
     );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -5,15 +5,15 @@ import Button from "@/components/button/Button";
 export default function Home() {
   return (
     <div className="flex items-center gap-24">
-      <div className="flex-1 space-y-10">
+      <div className="flex-1">
         <h1 className="text-7xl font-bold bg-gradient-to-b from-[#194c33] to-[#bbb] bg-clip-text text-transparent">Better design for your digital products.</h1>
-        <p className="text-2xl font-light">Turning your Idea into Reality. We bring together the teams from the
+        <p className="text-2xl font-light my-10">Turning your Idea into Reality. We bring together the teams from the
           global tech industry.</p>
 
         <Button url="/portfolio" text="See Our Works" />
       </div>
-      <div className="flex-1">
-        <Image className="w-4/5 object-cover mr-8 ml-auto" src={hero} alt="Home page hero" />
+      <div className="flex-1 h-[400px]">
+        <Image className="w-4/5 object-contain mr-8 ml-auto" src={hero} alt="Home page hero" />
       </div>
     </div>
   );
