6937a2ecae60a442d23f67ccd97dce0afb866b95
kawsar-uddin <kawsaruddin450@gmail.com>
2024-10-10 19:49:56 +0600

    implement blog details page

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import userPic from "@/public/1.png";
+import Image from 'next/image';
 
 const Post = () => {
     return (
         <div>
-            Blog Post
+            <div className="flex gap-12 items-center mt-12">
+                <div className="flex-grow flex flex-col gap-8">
+                    <h2 className="text-5xl font-bold">Blog Title</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt et in tenetur mollitia rerum, temporibus eligendi aut sit voluptatem iure excepturi dolores praesentium obcaecati! Eum, recusandae? Accusamus, necessitatibus autem.</p>
+                    <div className="flex gap-4 items-center">
+                        <Image className="rounded-full" src={userPic} alt="User Pic" />
+                        <h3 className="font-semibold">Kawsar Ahmed</h3>
+                    </div>
+                </div>
+                <div className="w-full h-[300px] relative">
+                    <Image className=" object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Blog Image" />
+                </div>
+            </div>
+            <div className="mt-12 text-justify">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia deleniti necessitatibus nobis voluptate temporibus corrupti, illo quidem assumenda? Recusandae quos optio modi eum neque, nulla, error, ipsum repellendus laboriosam vel obcaecati enim nemo est pariatur inventore facilis rerum? Quod, ipsum sunt accusantium veniam, ab pariatur eius temporibus voluptatibus nisi dolorem quam minus iusto dignissimos quibusdam voluptates praesentium alias fuga in laborum recusandae quaerat cumque vitae. Labore alias saepe tempora? Quae minus officia optio repellat, quaerat ducimus aperiam? Earum quasi quis quos delectus incidunt nesciunt esse nostrum beatae? Repudiandae minus similique quisquam dolores, sunt nulla nihil quam quidem ut repellendus, voluptate corporis ab praesentium laborum consectetur voluptatibus modi optio minima! Sunt praesentium ipsam magni, accusamus temporibus dignissimos quisquam vero inventore provident, consequuntur expedita pariatur, commodi veniam minus! Laborum pariatur saepe officiis numquam fugiat culpa ipsa, quasi voluptatibus iste nostrum sint ad rem neque quam earum maiores autem rerum, veritatis repudiandae corporis debitis maxime placeat atque consequatur. Harum similique mollitia eius minima quas numquam sint corrupti sed ipsa facere. Tempora atque iusto porro enim voluptate quod quas tenetur cum illo magnam soluta ab animi adipisci excepturi suscipit labore, non, magni sapiente. Unde necessitatibus dicta, sint eius culpa est impedit esse porro nihil.</p>
+            </div>
         </div>
     );
 };
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,29 @@
+import Button from '@/components/button/Button';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Blog = () => {
     return (
         <div>
-            Blog
+            <Link href="/blog/testId" className="flex gap-12 items-center my-8">
+                <div className="w-[500px] h-[300px] relative">
+                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image" />
+                </div>
+                <div className="flex-grow flex flex-col gap-8">
+                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
+                </div>
+            </Link>
+            <Link href="/blog/testId" className="flex gap-12 items-center my-8">
+                <div className="w-[500px] h-[300px] relative">
+                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image" />
+                </div>
+                <div className="flex-grow flex flex-col gap-8">
+                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
+                </div>
+            </Link>
         </div>
     );
 };
