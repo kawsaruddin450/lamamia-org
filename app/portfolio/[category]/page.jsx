62682fa35280ff23cf82f1ece913091d7bc4f47c
kawsar-uddin <kawsaruddin450@gmail.com>
2024-10-10 19:07:36 +0600

    copletes category subpage under portfolio page

--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,9 +1,41 @@
+import Button from '@/components/button/Button';
+import Image from 'next/image';
 import React from 'react';
 
-const Category = () => {
+const Category = ({params}) => {
     return (
         <div>
-            Category
+            <h1 className="my-5 text-2xl font-bold">{params.category}</h1>
+            <div className="flex gap-12 items-center">
+                <div className="flex-1 flex flex-col gap-8">
+                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
+                    <Button url="#" text="See More"/>
+                </div>
+                <div className="flex-1 h-[400px] relative">
+                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image"/>
+                </div>
+            </div>
+            <div className="flex flex-row-reverse gap-12 items-center">
+                <div className="flex-1 flex flex-col gap-8">
+                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
+                    <Button url="#" text="See More"/>
+                </div>
+                <div className="flex-1 h-[400px] relative">
+                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image"/>
+                </div>
+            </div>
+            <div className="flex gap-12 items-center">
+                <div className="flex-1 flex flex-col gap-8">
+                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
+                    <Button url="#" text="See More"/>
+                </div>
+                <div className="flex-1 h-[400px] relative">
+                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image"/>
+                </div>
+            </div>
         </div>
     );
 };
--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -6,13 +6,13 @@ const Portfolio = () => {
         <div>
             <h1 className="my-5 text-2xl font-bold">Choose a gallery</h1>
             <div className="flex gap-12 items-center">
-                <Link href="/portfolio/illustrations" className="border-4 border-[#bbb] w-[300px] h-[400px] rounded relative bg-illustrations-bg bg-no-repeat bg-cover bg-center">
+                <Link href="/portfolio/illustrations" className="border-4 border-[#bbb] w-[300px] h-[400px] rounded relative bg-illustrations-bg bg-no-repeat bg-cover bg-center hover:text-green-400">
                     <span className="absolute right-3 bottom-3 text-4xl font-bold">Illustrations</span>
                 </Link>
-                <Link href="/portfolio/websites" className="border-4 border-[#bbb] w-[300px] h-[400px] rounded relative bg-apps-bg bg-no-repeat bg-cover bg-center">
+                <Link href="/portfolio/websites" className="border-4 border-[#bbb] w-[300px] h-[400px] rounded relative bg-apps-bg bg-no-repeat bg-cover bg-center hover:text-green-400">
                     <span className="absolute right-3 bottom-3 text-4xl font-bold">Websites</span>
                 </Link>
-                <Link href="/portfolio/applications" className="border-4 border-[#bbb] w-[300px] h-[400px] rounded relative bg-websites-bg bg-no-repeat bg-cover bg-center">
+                <Link href="/portfolio/applications" className="border-4 border-[#bbb] w-[300px] h-[400px] rounded relative bg-websites-bg bg-no-repeat bg-cover bg-center hover:text-green-400">
                     <span className="absolute right-3 bottom-3 text-4xl font-bold">Applications</span>
                 </Link>
             </div>
