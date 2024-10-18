a2d3467efe665adf97da69ba2e49587d49efae6c
kawsar-uddin <kawsaruddin450@gmail.com>
2024-10-18 22:33:12 +0600

    fetch data from local json

--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,40 +1,37 @@
 import Button from '@/components/button/Button';
 import Image from 'next/image';
 import React from 'react';
+import { items } from './data';
+import { notFound } from 'next/navigation';
 
-const Category = ({params}) => {
+const Category = ({ params }) => {
+
+    const getData = (cat) => {
+        const data = items[cat];
+
+        if (data) {
+            return data;
+        }
+        return notFound();
+    }
+    const data = getData(params.category);
+
     return (
         <div>
             <h1 className="my-5 text-2xl font-bold">{params.category}</h1>
-            <div className="flex gap-12 items-center">
-                <div className="flex-1 flex flex-col gap-8">
-                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
-                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
-                    <Button url="#" text="See More"/>
-                </div>
-                <div className="flex-1 h-[400px] relative">
-                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image"/>
-                </div>
-            </div>
-            <div className="flex flex-row-reverse gap-12 items-center">
-                <div className="flex-1 flex flex-col gap-8">
-                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
-                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
-                    <Button url="#" text="See More"/>
-                </div>
-                <div className="flex-1 h-[400px] relative">
-                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image"/>
-                </div>
-            </div>
-            <div className="flex gap-12 items-center">
-                <div className="flex-1 flex flex-col gap-8">
-                    <h2 className="text-5xl font-bold">Creative Portfolio</h2>
-                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reprehenderit, sequi labore temporibus tempore consequuntur eius ratione numquam error, quis facere voluptatum? Expedita omnis molestias harum illum enim, ipsa deleniti!</p>
-                    <Button url="#" text="See More"/>
-                </div>
-                <div className="flex-1 h-[400px] relative">
-                    <Image className="w-full object-contain" fill={true} src="https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Portfolio Category Image"/>
-                </div>
+            <div>
+                {
+                    data?.map(item => <div key={item.id} className={`flex gap-12 items-center ${item.id %2 === 0 ? 'flex-row-reverse' : 'flex-row'} mb-12`}>
+                        <div className="flex-1 flex flex-col gap-8">
+                            <h2 className="text-5xl font-bold">{item.title}</h2>
+                            <p className="text-xl">{item.desc}</p>
+                            <Button url="#" text="See More" />
+                        </div>
+                        <div className="flex-1 h-[400px] relative">
+                            <Image className="w-full object-contain" fill={true} src={item.image} alt="Portfolio Category Image" />
+                        </div>
+                    </div>)
+                }
             </div>
         </div>
     );
